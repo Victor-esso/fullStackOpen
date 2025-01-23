762f92d95baf75225d5901f86225c408b46813a0
Victor Esso Nmakwe <victoresso97@gmail.com>
2025-01-23 14:57:24 +0100

    Done with Exercises 3.1.-3.6.

--- part4/phonebook-server/index.js
+++ part4/phonebook-server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const func = require('./functions')
 const app = express()
 const PORT = 3030
 
-contacts = [
+app.use(express.json());
+const contacts = [
     { 
       "id": "1",
       "name": "Arto Hellas", 
@@ -22,18 +24,55 @@ contacts = [
       "id": "4",
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
+    },
+    { 
+      "id": "5",
+      "name": "victor esso", 
+      "number": "09037871109"
     }
+    
 ]
 
 
 app.get('/api/contacts', (request , response) => {
     response.json(contacts)
 })
+
+app.post('/api/contacts', (request , response) => {
+    let id = func.getRandomInt(10,4000)
+    const newContact =  {id, ...request.body}
+
+    let duplicateName = contacts.find(contact => contact.name.toLowerCase().trim() === newContact.name.toLowerCase().trim())
+    let duplicateNumber = contacts.find(contact => contact.number.trim() === newContact.number.trim())
+    
+    duplicateName ? 
+        response.status(400).json({error : "Name must be unique"}) 
+        :
+        (duplicateNumber ? 
+            response.status(400).json({error : "Number must be unique"})
+            :
+            response.json([ newContact , ...contacts ])
+        )
+    
+})
+
+app.get('/api/contacts/:id', (request , response) => {
+    const id = request.params.id
+    const contact = contacts.find(contact => contact.id === id)
+    
+    contact ? response.json(contact) : response.status(404).json({status:"contact not found"})
+})
+
+app.delete('/api/contacts/:id', (request , response) => {
+    const id = request.params.id
+    const updatedContacts = contacts.filter(contact => contact.id !== id)
+    
+    response.status(204)
+})
+
 app.get('/info', (request , response) => {
-    // response.send('<h1>Victor is king</h1>')
-    response.send(`
-        Phonebook has info for ${contacts.length} people   
-    `)
+    const time = new Date()
+    response.send(`Phonebook has info for ${contacts.length} people <br/> <br/> ${time.toString()}`.trim())
 })
 
 
