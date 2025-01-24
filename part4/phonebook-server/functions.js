d183c00230c666e7d1a99b1e723d46e62425c213
Victor Esso Nmakwe <victoresso97@gmail.com>
2025-01-24 16:09:02 +0100

    Part4 Update

--- part4/phonebook-server/functions.js
+++
@@ -1,9 +0,0 @@
-
-function getRandomInt(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-}
-
-
-module.exports = { getRandomInt }
