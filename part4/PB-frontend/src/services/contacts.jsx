4649b581395ea8efd456e2a98cd4fb4950680659
Victor Esso Nmakwe <victoresso97@gmail.com>
2025-03-28 17:22:32 +0100

    contact Services

--- part4/PB-frontend/src/services/contacts.jsx
+++ part4/PB-frontend/src/services/contacts.jsx
@@ -7,18 +7,22 @@ const all = () => {
     const re = axios.get(baseUrl);
     return re.then(r => r.data);
 }
+
 const create = newContact => {
     const re = axios.post(baseUrl,newContact);
     return re.then(r => r.data);
 }
+
 const update = (id, newContact) => {
     const re = axios.put(`${baseUrl}/${id}`, newContact);
     return re.then(r => r.data);
 }
+
 const del = id => {
     const re = axios.delete(`${baseUrl}/${id}`);
     return re.then(r => r.data);
 }
+
 const clearAll = async () => {
     const allContacts = await axios.get(baseUrl).then(r => r.data);
     const deletePromises = allContacts.map(contact =>
