c34bed9d106038c7a5b314dca2ab3d346dc7ca21
Victor Esso Nmakwe <victoresso97@gmail.com>
2025-04-11 23:14:30 +0100

    Auto commit

--- part6/redux/src/pages/Home.jsx
+++ part6/redux/src/pages/Home.jsx
@@ -3,18 +3,18 @@ import { useDispatch ,  useSelector } from "react-redux"
 
 const Home = () => {
   const dispatch = useDispatch()
-
+  const { good , bad , ok } = useSelector(s => s.counter)
   return (
     <div>
       <div className="horizontal gap-2 *:bg-stone-600 *:rounded-sm *:px-5 *:py-2">
         <button onClick={() => dispatch(isGood())}>Good</button>
         <button onClick={() => dispatch(isBad())}>Bad</button>
         <button onClick={() => dispatch(isOk())}>Ok</button>
-        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={() => reset(500)}>Reset</button>
       </div>
-      <p>Good : {useSelector(s => s.good)}</p>
-      <p>Bad : {useSelector(s => s.bad)}</p>
-      <p>Ok : {useSelector(s => s.ok)}</p>
+      <p>Good : {good}</p>
+      <p>Bad : {bad}</p>
+      <p>Ok : {ok}</p>
     </div>
   )
 }
--- part6/redux/src/store.js
+++ part6/redux/src/store.js
@@ -1,38 +1,50 @@
 import { createStore } from "redux";
 
 const initialState = {
-    good : 0,
-    bad : 0,
-    ok : 0
+    counter : {
+        good : 0,
+        bad : 0,
+        ok : 0
+    }
 }
 
 // create actions
 const ADD_GOOD = 'ADD_GOOD'
 const ADD_BAD = 'ADD_BAD'
 const ADD_OK = 'ADD_OK'
+const RESETTING = 'RESETTING'
 const RESET = 'RESET'
 
 
-// actions 
-export const isGood = () => ({ type : ADD_GOOD})
-export const isBad = () => ({ type : ADD_BAD})
-export const isOk = () => ({ type : ADD_OK})
-export const reset = () => ({ type : RESET})
+
 
 const reducer = (state = initialState , action) => {
     switch (action.type) {
         case ADD_GOOD:
-            console.log({ ...state, good : state.good + 1})
-            return { ...state, good : state.good + 1}
+            return { ...state, counter : {...state.counter , good : state.counter.good + 1}}
         case ADD_BAD:
-            return { ...state, bad : state.bad + 1}
+            return { ...state, counter : {...state.counter , bad : state.counter.bad + 1}}
         case ADD_OK:
-            return { ...state, ok : state.ok + 1}
+            return { ...state, counter : {...state.counter , ok : state.counter.ok + 1}}
         case RESET:
             return initialState
+        case RESETTING:
+            return {...store , counter : {good : 'resetting ...' , bad : 'resetting ...' , ok: 'resetting ...'}}
         default:
             return state
     }
 }
 
-export default createStore(reducer)
+const store = createStore(reducer)
+// actions 
+export const isGood = () => ({ type : ADD_GOOD})
+export const isBad = () => ({ type : ADD_BAD})
+export const isOk = () => ({ type : ADD_OK})
+export const reset = (duration = 5000) => {
+    store.dispatch({ type : RESETTING})
+    setTimeout(() => {
+        store.dispatch({ type : RESET})
+    },duration)
+}
+
+export default store
